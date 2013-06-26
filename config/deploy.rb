c55d8aa33c1f3c30fe348cd954fed7e4715d8fc7
nuadha <sir_zaxg@hotmail.com>
2013-06-26 12:49:08 +0200

    added rvm_path

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,6 +4,7 @@ load 'deploy/assets'  # only for rails 3.1 apps, this makes sure our assets are 
 
 set :application, "phdadm"
 
+
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
@@ -15,10 +16,13 @@ set :scm, :git
 set :repository, "git@github.com:nuadha/phdadm.git"
 set :branch, "master"
 
+set :rvm_type, :system
+set :rvm_path, '/usr/local/rvm'
 set :user, "deploy"
 set :deploy_to, "/var/www/#{application}"
 set :deploy_via, :remote_cache
 set :use_sudo, false
+
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
